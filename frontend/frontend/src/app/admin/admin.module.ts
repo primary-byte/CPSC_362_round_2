e381519fdd1abe918ae1bba268a4bf76adc04d26
primary-byte <joshuathughes@csu.fullerton.edu>
2021-03-31 16:12:37 -0700

    working

--- frontend/frontend/src/app/admin/admin.module.ts
+++ frontend/frontend/src/app/admin/admin.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { SubNavComponent } from './subnav.component';
-import { LayoutComponent } from '../profile/layout.component';
+import { LayoutComponent } from './layout.component';
 import { OverviewComponent } from './overview.component';
 
 @NgModule({
